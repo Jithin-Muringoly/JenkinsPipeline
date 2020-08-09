f8639c7a6647a564967be76cc3d584836b3cd0d7
Jithin-Muringoly <60697964+Jithin-Muringoly@users.noreply.github.com>
2020-08-09 21:41:15 +0530

    Update container.sh

--- container.sh
+++ container.sh
@@ -4,6 +4,9 @@ echo "build done from container.sh"
 ls -ltr               
 docker run --name 060800 customdocker:5.0 -no-cache
 docker ps -a | awk 'NR > 1 {print $1; exit}'
+echo "startiung conatiner 060800"
+docker start 060800
+echo "contianer 060800 started"
 docker ps --filter ancestor="customdocker:5.0" --format "{{.ID}}"
 docker ps --filter ancestor=customdocker:5.0 --format "{{.ID}}"
 conatinerId1=$(docker ps -a | awk 'NR > 1 {print $1; exit}')
